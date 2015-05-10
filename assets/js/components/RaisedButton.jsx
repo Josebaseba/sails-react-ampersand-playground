89d7f97f73f712ad4dc9b46a5671db240582dedc
Joseba Legarreta <josebalega@gmail.com>
2015-05-10 20:55:58 +0200

    Raised button text changed

--- assets/js/components/RaisedButton.jsx
+++ assets/js/components/RaisedButton.jsx
@@ -8,7 +8,7 @@ module.exports = React.createClass({
 
   render: function() {
     return (
-      <RaisedButton label="Primary" primary={true} />
+      <RaisedButton label="First section button" primary={true} />
     );
   }
 
